c28e8ca44f2433dc6e4bc4f5cb960962a397ac0b
Zhanna Oliferova <zoliferova@gmail.com>
2024-12-25 19:11:34 +0300

    Update lib.rs

--- learn_game/src/lib.rs
+++ learn_game/src/lib.rs
@@ -74,7 +74,6 @@ impl Game {
         self.board.current_state = self.board.next_state.clone();
     }
     fn learn_episode(&mut self, q: Option<&mut QTable>) {
-        let mut draw_counter = 0_i32;
         let q = q.expect("QTable is initialized and should be valid at this point.");
         self.board = Board::new();
         self.assign_players();
@@ -87,13 +86,6 @@ impl Game {
                 } else {
                     self.current_player.choose_move(&self.board, q)
                     };
-            if self.episode % 100_000 == 0 {
-                self.board.current_state.draw();
-                println!("Current_player {:?} mark {:?}", self.current_player.get_name(),
-                    self.current_player.get_mark());
-                println!("Initial state: {:?}", q[&current_state_key]);
-                println!("the current player chose move {:?}", &mv);
-            }
             self.current_player.make_move(&mut self.board, &mv);
             let next_state_key = self.board.next_state.to_state_key() + self.other_player.get_name();
             q.entry(next_state_key.clone())
@@ -113,7 +105,6 @@ impl Game {
                     self.swap_states();
                 }
                 IsGameOver::Drawn => {
-                    draw_counter += 1;
                     q.update_q_table(
                         &current_state_key,
                         &next_state_key,
@@ -122,13 +113,6 @@ impl Game {
                         0.0,
                         true,
                     );
-                    if self.episode % 100_000 == 0 {
-                        println!("Episode {:?}, total draws {:?}", self.episode, draw_counter);
-                        // println!("Game drawn");
-                        // println!("Exit from episode, current player and its mark, qtable['---------'] {:?}, {:?}, {:?}",
-                        //     self.current_player.get_name(), self.current_player.get_mark(),
-                        //     q[&("---------".to_owned() + self.current_player.get_name())]);
-                    }
                     self.episode += 1;
                     break;
                 }
@@ -139,14 +123,6 @@ impl Game {
                     } else {
                         reward = -1.0;
                     }
-                    // if self.episode % 1 == 0 {
-                    //     println!(
-                    //        "Winner: {:?}, reward: {:?}",
-                    //        self.current_player.get_name(),
-                    //        reward
-                    //     );
-                    //     println!("After win QTable before update {:?}", q.get(&current_state_key));
-                    // }
                     q.update_q_table(
                         &current_state_key,
                         &next_state_key,
@@ -155,12 +131,6 @@ impl Game {
                         reward,
                         true,
                     );
-                    // if self.episode % 1 == 0 {
-                    //     println!("After win QTable after update {:?}", q.get(&current_state_key));
-                    //     // println!("Exit from episode, current player and its mark, qtable['---------'] {:?}, {:?}, {:?}, {:?}",
-                    //     //     self.episode, self.current_player.get_name(), self.current_player.get_mark(),
-                    //     //     q[&("---------".to_owned() + self.current_player.get_name())]);
-                    // }
                     self.episode += 1;
                     break;
                 }
@@ -180,13 +150,6 @@ impl Game {
                 } else {
                     self.current_player.choose_move(&self.board, q)
                     };
-            if self.episode % 10_000 == 0 {
-                self.board.current_state.draw();
-                println!("Current_player {:?} mark {:?}", self.current_player.get_name(),
-                    self.current_player.get_mark());
-                println!("Initial state: {:?}", q[&current_state_key]);
-                println!("the current player chose move {:?}", &mv);
-            }
             self.current_player.make_move(&mut self.board, &mv);
             let next_state_key = self.board.next_state.to_state_key() + self.other_player.get_name();
             q.entry(next_state_key.clone())
@@ -214,12 +177,6 @@ impl Game {
                         0.0,
                         true,
                     );
-                    if self.episode % 10_000 == 0 {
-                        println!("Game drawn");
-                        println!("Exit from episode, current player and its mark, qtable['---------'] {:?}, {:?}, {:?}",
-                            self.current_player.get_name(), self.current_player.get_mark(),
-                            q[&("---------".to_owned() + self.current_player.get_name())]);
-                    }
                     self.episode += 1;
                     break;
                 }
@@ -230,14 +187,6 @@ impl Game {
                     } else {
                         reward = -1.0;
                     }
-                    if self.episode % 10_000 == 0 {
-                        println!(
-                           "Winner: {:?}, reward: {:?}",
-                           self.current_player.get_name(),
-                           reward
-                        );
-                        println!("After win QTable before update {:?}", q.get(&current_state_key));
-                    }
                     q.update_q_table(
                         &current_state_key,
                         &next_state_key,
@@ -246,12 +195,6 @@ impl Game {
                         reward,
                         true,
                     );
-                    if self.episode % 10_000 == 0 {
-                        println!("After win QTable after update {:?}", q.get(&current_state_key));
-                        println!("Exit from episode, current player and its mark, qtable['---------'] {:?}, {:?}, {:?}, {:?}",
-                            self.episode, self.current_player.get_name(), self.current_player.get_mark(),
-                            q[&("---------".to_owned() + self.current_player.get_name())]);
-                    }
                     self.episode += 1;
                     break;
                 }
@@ -261,11 +204,9 @@ impl Game {
     fn learn_q_table(&mut self, mut q: Option<&mut QTable>) {
         loop {
             if self.episode > 100_000 && self.episode % 10_000 == 0 {
-                println!("{:?} {:?}", self.episode, NUM_EPISODES);
                 if EXPLORATION_RATE.with_borrow(|erate| -> bool {*erate > 0.1}) {
                     EXPLORATION_RATE.with_borrow_mut(|erate| {
                         *erate -= 0.1;
-                        println!("exploration rate is {:?}", erate);
                     });
                 }
             }
@@ -282,24 +223,20 @@ impl Game {
             self.learn_episode(q.as_deref_mut());
         }
         let path = Path::new("./q_table_archive/");
-        println!("QTable after exit: {:?}", q);
         let is_q_saved = q_table::q_table_to_disk(&path, q.as_deref().unwrap());
     }
     fn learn_q_table_with_minimax(&mut self, mut q: Option<&mut QTable>) {
         loop {
             if self.episode > 5_000 && self.episode % 1_000 == 0 {
-                println!("{:?} {:?}", self.episode, NUM_EPISODES);
                 if EXPLORATION_RATE.with_borrow(|erate| -> bool {*erate > 0.1}) {
                     EXPLORATION_RATE.with_borrow_mut(|erate| {
                         *erate -= 0.1;
-                        println!("exploration rate is {:?}", erate);
                     });
                 }
             }
             if self.episode > 10_000 && self.episode % 5_000 == 0 {
                 K.with_borrow_mut(|k| {
                     *k += 0.05;
-                    println!("K value is {:?} after episode {:?}", k, self.episode);
                 });
             }
             if self.episode >= NUM_EPISODES {
@@ -309,7 +246,6 @@ impl Game {
             self.learn_episode_with_minimax(q.as_deref_mut());
         }
         let path = Path::new("./q_table_archive/");
-        println!("QTable at the exit: {:?}", q);
         let is_q_saved = q_table::q_table_to_disk(&path, q.as_deref().unwrap());
     }
 }
@@ -364,8 +300,6 @@ pub fn play_game_2_humans() {
         if game.board.is_full() {break;}
         let mv = game.current_player.choose_move(&game.board, &mut q);
         game.current_player.make_move(&mut game.board, &mv);
-        // println!("Drawing next state");
-        // game.board.next_state.draw();
         let is_over = game.board.is_game_over(&game.current_player.get_mark());
         match is_over {
             IsGameOver::InPlay => {
@@ -396,17 +330,13 @@ pub fn play_game_human_computer_player() {
         name: "RLmax".to_owned(),
         mark: Marks::None,
     });
-    println!("Player_1 is {:?}", &player_1);
     let mut game = Game::new(player_1, player_2);
     let path = Path::new("./q_table_archive/qtable");
     let mut q = q_table::q_table_from_disk_pickle(&path).expect("QTable is always present");
     println!("QTable's length is: {:?}", q.len());
     loop {
-        //if game.board.is_full() {break;}
         let mv = game.current_player.choose_move(&game.board, &mut q);
         game.current_player.make_move(&mut game.board, &mv);
-        // println!("Drawing next state");
-        // game.board.next_state.draw();
         let is_over = game.board.is_game_over(&game.current_player.get_mark());
         match is_over {
             IsGameOver::InPlay => {
@@ -440,18 +370,11 @@ pub fn play_human_minimax() {
         mark: player_1.mark.other(),
         q_max,
     });
-    println!("Player_1 is {:?}", &player_1);
-    println!("Player_2 is {:?}", &player_2);
     let mut game = Game::new(player_1, player_2);
-    // let path = Path::new("./q_table_archive/qtable");
-    // let q = q_table::q_table_from_disk_pickle(&path).expect("QTable is always present");
-    // println!("QTable's length is: {:?}", q.len());
     let mut q = QTable::new();
     loop {
         let mv = game.current_player.choose_move(&game.board, &mut q);
         game.current_player.make_move(&mut game.board, &mv);
-        // println!("Drawing next state");
-        // game.board.next_state.draw();
         let is_over = game.board.is_game_over(&game.current_player.get_mark());
         match is_over {
             IsGameOver::InPlay => {
@@ -473,7 +396,6 @@ pub fn play_human_minimax() {
             }
         }
      }
-     println!("The QTable is {:?}", q);
 }
 
 #[cfg(test)]
